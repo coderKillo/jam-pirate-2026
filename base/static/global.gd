c2cb3ecd1bd5dbe171b4591a8dbf78ce9036aeee
chmu7568 <christian.mueller@esolutions.de>
2026-01-29 23:45:35 +0100

    make levels

--- base/static/global.gd
+++ base/static/global.gd
@@ -3,3 +3,5 @@ extends Node
 
 const WORLD_LAYER = 1
 const VIRTUAL_LAYER = 2
+const REAL_DMG_LAYER = 4
+const VIRTUAL_DMG_LAYER = 8
--- game/world/window/window_area.gd
+++ game/world/window/window_area.gd
@@ -12,9 +12,19 @@ func _on_body_entered(body: PhysicsBody2D):
 	body.set_collision_mask_value(Global.VIRTUAL_LAYER, true)
 	body.set_collision_mask_value(Global.WORLD_LAYER, false)
 
+	var hurtbox = body.get_node_or_null("Hurtbox")
+	if hurtbox:
+		hurtbox.set_collision_mask_value(Global.VIRTUAL_DMG_LAYER, true)
+		hurtbox.set_collision_mask_value(Global.REAL_DMG_LAYER, false)
+
 
 func _on_body_exited(body: PhysicsBody2D):
 	body.set_collision_layer_value(Global.VIRTUAL_LAYER, false)
 	body.set_collision_layer_value(Global.WORLD_LAYER, true)
 	body.set_collision_mask_value(Global.VIRTUAL_LAYER, false)
 	body.set_collision_mask_value(Global.WORLD_LAYER, true)
+
+	var hurtbox = body.get_node_or_null("Hurtbox")
+	if hurtbox:
+		hurtbox.set_collision_mask_value(Global.VIRTUAL_DMG_LAYER, false)
+		hurtbox.set_collision_mask_value(Global.REAL_DMG_LAYER, true)
